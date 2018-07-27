7d82851c2d07299ecb5b62fc4c037348ebdb0228
Luís Ferreira <contact@lsferreira.net>
2018-07-27 17:17:40 +0100

    build: update build commands
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- build/env.sh
+++ build/env.sh
@@ -30,5 +30,6 @@
 # For more info about intellectual property visit: aurorafoss.org or
 # directly send an email to: contact (at) aurorafoss.org .
 
+function p() { meson . .build }
 function b() { cd .build && ninja && cd .. }
 function c() { cd .build && ninja clean && cd .. }
