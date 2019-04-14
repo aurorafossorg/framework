d535954f8369df2c5dcbc3b052a66308aaf85275
Luís Ferreira <contact@lsferreira.net>
2019-04-14 15:34:52 +0100

    tools: rename dub script for general commands
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

---
+++ tools/dub.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+dub $@
+dub $@ :android
+dub $@ :audio
+dub $@ :cli
+dub $@ :core
+dub $@ :graphics
+dub $@ :gui
+dub $@ :image
+dub $@ :jni
+dub $@ :math
+dub $@ :metadata
+dub $@ :net
+dub $@ :stdx
