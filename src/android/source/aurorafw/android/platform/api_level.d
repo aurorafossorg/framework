7a7c9bb48e89d6187e6ad1f596af3fc1788ae287
Luís Ferreira <contact@lsferreira.net>
2019-04-14 19:00:16 +0100

    android: add documentation and update to android q
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- src/android/source/aurorafw/android/platform/api_level.d
+++ src/android/source/aurorafw/android/platform/api_level.d
@@ -39,32 +39,67 @@ Project implementation. Check it out at android.googlesource.com .
 
 module aurorafw.android.platform.api_level;
 
+/**
+ * @file aurorafw/android/platform/api_level.d
+ * @brief Functions and constants for dealing with multiple API levels.
+ */
+
+
 version (Android):
 extern (C):
 @system:
 nothrow:
 @nogc:
 
-/*
- * Magic version number for a current development build, which has
- * not yet turned into an official release.
+/**
+ * Magic version number for an Android OS build which has
+ * not yet turned into an official release,
+ * for comparisons against __ANDROID_API__.
  */
-
 enum __ANDROID_API_FUTURE__ = 10000;
 
+/**
+ * `__ANDROID_API__` is the API level being targeted. For the OS,
+ * this is `__ANDROID_API_FUTURE__`. For the NDK, this is set by the
+ * compiler/build system based on the API level you claimed to target.
+ */
 enum __ANDROID_API__ = __ANDROID_API_FUTURE__;
 
-enum __ANDROID_API_G__ = 9;
-enum __ANDROID_API_I__ = 14;
-enum __ANDROID_API_J__ = 16;
-enum __ANDROID_API_J_MR1__ = 17;
-enum __ANDROID_API_J_MR2__ = 18;
-enum __ANDROID_API_K__ = 19;
-enum __ANDROID_API_L__ = 21;
-enum __ANDROID_API_L_MR1__ = 22;
-enum __ANDROID_API_M__ = 23;
-enum __ANDROID_API_N__ = 24;
-enum __ANDROID_API_N_MR1__ = 25;
-enum __ANDROID_API_O__ = 26;
-enum __ANDROID_API_O_MR1__ = 27;
-enum __ANDROID_API_P__ = 28;
+enum __ANDROID_API_G__ = 9; /** Names the Gingerbread API level (9), for comparisons against __ANDROID_API__. */
+enum __ANDROID_API_I__ = 14; /** Names the Ice-Cream Sandwich API level (14), for comparisons against __ANDROID_API__. */
+enum __ANDROID_API_J__ = 16; /** Names the Jellybean API level (16), for comparisons against __ANDROID_API__. */
+enum __ANDROID_API_J_MR1__ = 17; /** Names the Jellybean MR1 API level (17), for comparisons against __ANDROID_API__. */
+enum __ANDROID_API_J_MR2__ = 18; /** Names the Jellybean MR2 API level (18), for comparisons against __ANDROID_API__. */
+enum __ANDROID_API_K__ = 19; /** Names the KitKat API level (19), for comparisons against __ANDROID_API__. */
+enum __ANDROID_API_L__ = 21; /** Names the Lollipop API level (21), for comparisons against __ANDROID_API__. */
+enum __ANDROID_API_L_MR1__ = 22; /** Names the Lollipop MR1 API level (22), for comparisons against __ANDROID_API__. */
+enum __ANDROID_API_M__ = 23; /** Names the Marshmallow API level (23), for comparisons against __ANDROID_API__. */
+enum __ANDROID_API_N__ = 24; /** Names the Nougat API level (24), for comparisons against __ANDROID_API__. */
+enum __ANDROID_API_N_MR1__ = 25; /** Names the Nougat MR1 API level (25), for comparisons against __ANDROID_API__. */
+enum __ANDROID_API_O__ = 26; /** Names the Oreo API level (26), for comparisons against __ANDROID_API__. */
+enum __ANDROID_API_O_MR1__ = 27; /** Names the Oreo MR1 API level (27), for comparisons against __ANDROID_API__. */
+enum __ANDROID_API_P__ = 28; /** Names the Pie API level (28), for comparisons against __ANDROID_API__. */
+enum __ANDROID_API_Q__ = 29; /** Names the "Q" API level (29), for comparisons against __ANDROID_API__. */
+
+/**
+ * Returns the `targetSdkVersion` of the caller, or `__ANDROID_API_FUTURE__`
+ * if there is no known target SDK version (for code not running in the
+ * context of an app).
+ *
+ * The returned values correspond to the named constants in `<android/api-level.h>`,
+ * and is equivalent to the AndroidManifest.xml `targetSdkVersion`.
+ *
+ * See also android_get_device_api_level().
+ *
+ * Available since API level 24.
+ */
+int android_get_application_target_sdk_version();
+
+/**
+ * Returns the API level of the device we're actually running on, or -1 on failure.
+ * The returned values correspond to the named constants in `<android/api-level.h>`,
+ * and is equivalent to the Java `Build.VERSION.SDK_INT` API.
+ *
+ * See also android_get_application_target_sdk_version().
+ */
+int android_get_device_api_level();
