c7f1ea0b6f10b878c72df433bb9490f4b81023cd
Luís Ferreira <contact@lsferreira.net>
2019-09-28 13:39:04 +0100

    tools: dub: fix reporting bad coverage and improve modules list
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- tools/dub.sh
+++ tools/dub.sh
@@ -2,25 +2,46 @@
 
 set -e
 
-dub $@
+dub "$@"
 
-PACKAGES="
-core
-cli
-math
-audio
-image
-graphics
-gui
-net
-metadata
-stdx
+PACKAGES="$(find src -mindepth 1 -maxdepth 1 -type d -exec basename {} \; | grep -v -E "unit|source")"
+
+
+coverage_tmp_folder=".dub-coverage"
+
+if [[ "$*" == *"--coverage"* ]]
+then
+	mkdir -p $coverage_tmp_folder
+	DUB_COVERAGE="1"
+fi
+
+exit_trap() {
+    if [[ $DUB_COVERAGE == "1" ]]
+	then
+		USELESS_DUB_COVERAGE_OUTPUT="
+-home-
+-root-
+-tmp-
 "
+		for useless in $USELESS_DUB_COVERAGE_OUTPUT; do
+			rm -rf -- "$useless"*.lst
+		done
+
+		mv -f "$coverage_tmp_folder"/* .
+		rm -rf "$coverage_tmp_folder"
+	fi
+}
+
+trap exit_trap EXIT
 
 for subprojects in $PACKAGES; do
-	dub $@ :$subprojects
+	dub "$@" ":$subprojects"
+	if [[ "$DUB_COVERAGE" == "1" ]]
+	then
+		mv "src-$subprojects-"*.lst "$coverage_tmp_folder"
+	fi
 done
 
 if [ "$1" == "test" ]; then
-	dub $@ :unit
+	dub "$@" :unit
 fi
